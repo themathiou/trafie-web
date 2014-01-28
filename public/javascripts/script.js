318a3b54cdde40b4c43e8ae3f7218d3c7dbf56c2
Theodore Mathioudakis <tmathioudakis@gmail.com>
2014-01-28 22:18:49 +0200

    working on profile page

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -14,6 +14,27 @@ function cancelEditField (fieldID_shadow, fieldID, saveID, cancelID, editID ) {
     document.getElementById(editID).style.display = 'block';
 }
 
+
+function show_specific_form(choice) {
+    var distance = ['high_jump','long_jump','triple_jump', 'pole_vault', 'shot_put', 'discus', 'hammer', 'javelin'];
+    var time = ['100m', '200m', '400m', '800m', '1500m', '3000m', '60m_hurdles', '100m_hurdles', '110m_hurdles', '400m_hurdles', '3000m_steeple', '4x100m_relay', '4x400m_relay', 'marathon'];
+    var points = ['pentathlon', 'heptathlon', 'decathlon'];
+    
+    console.log('Choice: ' + choice.value);
+
+    if( choice.value in distance ) {
+        console.log('distance');
+    }
+    else if ( choice.value in time ) {
+        console.log('time');
+    } 
+    else if ( choice.value in points ) {
+        console.log('points');
+    }
+    else{
+        console.log('Shit! Fuck! Shit!'); //TO BE REMOVED!
+    } 
+}
 	
 // var distance = ['high_jump','long_jump','triple_jump', 'pole_vault', 'shot_put', 'discus', 'hammer', 'javelin'];
 // var time = ['100m', '200m', '400m', '800m', '1500m', '3000m', '60m_hurdles', '100m_hurdles', '110m_hurdles', '400m_hurdles', '3000m_steeple', '4x100m_relay', '4x400m_relay', 'marathon'];
