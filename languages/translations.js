1caaa3ca6197f82b87715cc796cbda8020031e90
geobalas <geobal87@yahoo.gr>
2014-03-29 21:09:18 +0200

    Added pr language

--- languages/translations.js
+++ languages/translations.js
@@ -2,5 +2,6 @@ var translations = [];
 translations['en'] = require('../languages/en.js');
 translations['el'] = require('../languages/el.js');
 translations['ru'] = require('../languages/ru.js');
+translations['pr'] = require('../languages/pr.js');
 
 module.exports = translations;
