8a64bf0d01f4ab1251e31dd32b0413e02d78dd68
Theodore Mathioudakis <tmathioudakis@gmail.com>
2014-04-08 22:11:20 +0300

    minor fixes on statistics.js

--- public/javascripts/statistics.js
+++ public/javascripts/statistics.js
@@ -1,10 +1,12 @@
+/*
 google.load("visualization", "1", {packages:["corechart"]});
 google.setOnLoadCallback(drawChart);
+*/
 
 function drawChart( user_id, discipline ) {
 	console.log(user_id, discipline);
-	var req_data = { discipline: discipline };
-	ajax_get( req_data, '/user/' + user_id + '/activities', function( res_status, res_data ){
+
+	ajax_get( '/user/' + user_id + '/activities?discipline='+discipline, function( res_status, res_data ){
 		console.log(res_data, data);
 
 		var data = google.visualization.DataTable(res_data);
