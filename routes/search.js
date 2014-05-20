a66d5219cb97e002a66b06df597c0b9d6c10739e
geobalas <geobal87@yahoo.gr>
2014-05-20 22:45:26 +0300

    Returning the users translated discipline in search

--- routes/search.js
+++ routes/search.js
@@ -8,12 +8,14 @@ exports.get = function( req, res ) {
 		res.redirect('/');
 	}
 	
+	var language;
 	// Find the profile
 	Profile.schema.findOne({ '_id': req.session.user_id }, 'language date_format')
 	.then( function( profile_data ) {
 		var requested_value_string = req.query.value.trim();
 		var requested_values = requested_value_string.split(' ');
 		var requested_values_length = requested_values.length;
+		language = profile_data.language;
 
 		if( !requested_value_string ) {
 			res.json( [] );
@@ -36,7 +38,21 @@ exports.get = function( req, res ) {
 		return Profile.schema.find( query, 'first_name last_name discipline country username _id', 10 );
 	})
 	.then( function( results ){
-		res.json( results );
+		var results_length = results.length;
+		var formatted_results = [];
+		for ( var i=0 ; i<results_length ; i++ ) {
+			formatted_results[i] = {};
+			formatted_results[i]._id = results[i]._id;
+			formatted_results[i].first_name = results[i].first_name;
+			formatted_results[i].last_name = results[i].last_name;
+			formatted_results[i].discipline = results[i].discipline;
+			formatted_results[i].country = results[i].country;
+			formatted_results[i].username = results[i].username;
+			formatted_results[i].formatted_country = results[i].country ? translations[language][results[i].country] : '';
+			formatted_results[i].formatted_discipline = results[i].discipline ? translations[language][results[i].discipline] : '';
+		}
+
+		res.json( formatted_results );
 	})
 	.fail( function( error ) {
 		send_error_page( error, res );
