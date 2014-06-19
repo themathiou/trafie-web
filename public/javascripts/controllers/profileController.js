4e565754792ca56b0fd29a7f59d9a48bfd90cf10
Theodore Mathioudakis <tmathioudakis@gmail.com>
2014-06-19 22:08:26 +0300

    profile: post new activities. delete activity.

--- public/javascripts/controllers/profileController.js
+++ public/javascripts/controllers/profileController.js
@@ -1,5 +1,5 @@
 trafie.controller("profileController", function( $rootScope, $scope, $http ){
-	//GENERAL VIARABLES
+	//GENERAL VARIABLES
 	$scope.disciplines = {
       'time': [
         '100m',
@@ -34,6 +34,10 @@ trafie.controller("profileController", function( $rootScope, $scope, $http ){
       ]
     };
 
+    //time form
+    $scope.newActivityForm = {};
+
+
     $scope.initProfile = function(){
     	$http.get('/profile/'+ $rootScope.user._id)
 		.success(function(res){
@@ -49,9 +53,6 @@ trafie.controller("profileController", function( $rootScope, $scope, $http ){
 		});
     }
 
-	
-
-
 	$scope.getActivities = function(user_id, discipline){
 		$http.get('/user/' + user_id + '/activities?discipline=' + discipline)
 		.success(function(res){
@@ -59,5 +60,40 @@ trafie.controller("profileController", function( $rootScope, $scope, $http ){
 		});
 	};
 	
+	/*
+	submitNewActivity function : creates the object for new activity submission and makes the ajax call (POST)
+	@param type : the type of the discipline ( accepted values : time, distance, points ) 
+	 */
+	$scope.submitNewActivity = function(){
+				
+				var data = $scope.newActivityForm;
+				data.discipline = data.selected_discipline.id;
+				/* TO BE REMOVED - ADDS A TEMP TODAY DATE */
+				var tmp = new Date();
+				var splitDate = tmp.toString().split(' ');
+				data.date = splitDate[0] + ' ' + splitDate[1] + ' ' +splitDate[2] + ' ' +splitDate[3]; 
 
+				console.log(data);
+				
+				$http.post('/user/' + $rootScope.user._id + '/activities', data)
+				.success(function(res){
+					console.log(res);
+					$scope.activities.push(res);
+				})
+				.error(function(e){});
+	}
+
+	$scope.deleteActivity = function( activity_id ){
+		$http.delete( '/user/' + $rootScope.user._id +'/activities/' + activity_id )
+		.success(function(res){
+			for( var i in $scope.activities ) {
+				if( $scope.activities[i]._id == activity_id ){
+					$scope.activities.splice(i,1);
+					break;
+				}
+			}
+		})
+		.error(function(e){})
+		
+	}
 });
