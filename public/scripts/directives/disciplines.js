33482010803eb9bd4a978dee29b303c7218ee5d0
Theodore Mathioudakis <tmathioudakis@gmail.com>
2014-07-22 20:48:11 +0300

    remove discipline-filtering-directive. Add upload-file. minor fixes

--- public/scripts/directives/disciplines.js
+++ public/scripts/directives/disciplines.js
@@ -2,28 +2,16 @@
 //---
 //Disciplines of the user
 trafie.directive("trfActiveDisciplines", function( $http ){
-
-	function getActivities(user_id, discipline){
-		var url =  '';
-		discipline != '' ? url = '/user/' + user_id + '/activities?discipline=' + discipline : url = '/user/' + user_id + '/activities';
-		$http.get(url)
-		.success(function(res){
-			$scope.activities = res;
-		})
-	}
-
 	return {
-		restrict:'E',
-		scope:{
-			user_id:'@'
-		},
+		restrict:'A',
 		link: function(scope, element, attrs){
-			$http.get('/user/'+scope.user_id+'/disciplines')
+			$http.get('/user/'+attrs.user_id+'/disciplines')
 			.success(function(res){
-				scope.response = res;
-				scope.response.id = attrs.userId;
-			})
+				scope.disciplines = res;
+				console.log(scope.disciplines);
+			});
 		},
-		template: '<ul><li ng-repeat="discipline in response" style="float:left;"><a ng-click="getActivities(\'{{ response.id }}\', \'{{ discipline.discipline }}\')">{{discipline.formatted_discipline}}</a></li></ul>'
+		template: '<ul><li ng-repeat="discipline in disciplines" style="float:left;"><a ng-click="getActivities(\'{{current_user._id}}\', \'{{discipline.discipline}}\')">{{discipline.formatted_discipline}}</a></li></ul>',
+		
 	}
 })
