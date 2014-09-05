0b0fbfbe9836a3d175db209af903040f822a2802
mathiou <root@Thodoriss-MacBook-Pro.local>
2014-09-05 23:44:56 +0300

    working on view other profiles

--- public/scripts/controllers/profileController.js
+++ public/scripts/controllers/profileController.js
@@ -1,13 +1,14 @@
 trafie.controller("profileController", function(
-	$rootScope,
-	$scope,
-	$http,
-	$timeout,
-	$q,
-	$routeParams ){
+  $rootScope,
+  $scope,
+  $http,
+  $timeout,
+  $window,
+  $q,
+  $routeParams ){
 
-	//GENERAL VARIABLES
-	$scope.disciplines = {
+  //GENERAL VARIABLES
+  $scope.disciplines = {
       'time': [
         '100m',
         '200m',
@@ -43,198 +44,170 @@ trafie.controller("profileController", function(
 
     //variable for Open/Close accordions
     $scope.accordions = {
-    	addActivity : false
+      addActivity : false
     }
 
     //time form
     $scope.newActivityForm = {};
 
-
-
     $scope.initProfile = function(){
-    	//true if this is the profile of the logged-in user
-		$scope.self = false;
+      //true if this is the profile of the logged-in user
+    $scope.self = false;
+    $scope.page_not_found = false;
 
-		if( $routeParams.userID ) {
-			$scope.getProfile( $routeParams.userID );
-			$routeParams.userID !== $rootScope.user._id ? $scope.self = false : $scope.self = true;
-		}
-		else {
-			$scope.getProfile( $rootScope.user._id );
-			$scope.self = true;
-		}
-	}
+    if( $routeParams.userID ) {
+      $scope.getProfile( $routeParams.userID );
+      $routeParams.userID !== $rootScope.user._id ? $scope.self = false : $scope.self = true;
+    }
+    else {
+      $scope.getProfile( $rootScope.user._id );
+      $scope.self = true;
+    }
+  }
 
-	//get user profile based on user id
-	$scope.getProfile = function( user_id ){
-		console.log( 'user_id in get profile:' + user_id );
-		$http.get('/profile/'+ user_id)
-		.success(function(res){
-			$rootScope.current_user = res;
-			$rootScope.disciplines_options = [];
-			for( i in res.disciplines ) {
-				var temp = { name: res.disciplines[i] , id: i };
-				$rootScope.disciplines_options.push(temp);
-			}
+  //get user profile based on user id
+  $scope.getProfile = function( user_id ){
+    console.log( 'user_id in get profile:' + user_id );
+    $http.get('/profile/'+ user_id)
+    .success(function(res){
+      $rootScope.current_user = res;
+      $rootScope.disciplines_options = [];
+      for( i in res.disciplines ) {
+        var temp = { name: res.disciplines[i] , id: i };
+        $rootScope.disciplines_options.push(temp);
+      }
 
-			console.log( 'user', $rootScope.user );
-			console.log( 'current_user', $rootScope.current_user );
+      console.log( 'user', $rootScope.user );
+      console.log( 'current_user', $rootScope.current_user );
 
-			//get user's activities
-			$scope.getActivities( user_id , $rootScope.current_user.discipline);
+      //get user's activities
+      $scope.getActivities( user_id , $rootScope.current_user.discipline);
 
-		})
-		.error( function(res) {
-			console.log( 'info :: User not found. Maybe he doesn\'t have a trafie profile or the profile is private.');
-		});
-	}
+    })
+    .error( function(res) {
+      $scope.page_not_found = true;
+      console.log( 'info :: User not found. Maybe he doesn\'t have a trafie profile or the profile is private.');
+    });
+  }
 
-	//get user activities based on user id
-	$scope.getActivities = function(user_id, discipline){
-		var url =  '';
-		discipline != '' ? url = '/user/' + user_id + '/activities?discipline=' + discipline : url = '/user/' + user_id + '/activities';
-		$http.get(url)
-		.success(function(res){
-			$scope.activities = res;
-		})
-	}
+  //get user activities based on user id
+  $scope.getActivities = function(user_id, discipline){
+    var url =  '';
+    discipline != '' ? url = '/user/' + user_id + '/activities?discipline=' + discipline : url = '/user/' + user_id + '/activities';
+    $http.get(url)
+    .success(function(res){
+      $scope.activities = res;
+    })
+  }
 
-	/*
-	submitNewActivity function : creates the object for new activity submission and makes the ajax call (POST)
-	@param type : the type of the discipline ( accepted values : time, distance, points )
-	 */
-	$scope.submitNewActivity = function(){
-		var data = $scope.newActivityForm;
-		data.discipline = data.selected_discipline.id;
-		console.log('add activity: ', data.date);
-		var splitDate = data.date.toString().split(' ');
-		data.date = splitDate[0] + ' ' + splitDate[1] + ' ' +splitDate[2] + ' ' +splitDate[3];
+  /*
+  submitNewActivity function : creates the object for new activity submission and makes the ajax call (POST)
+  @param type : the type of the discipline ( accepted values : time, distance, points )
+   */
+  $scope.submitNewActivity = function(){
+    var data = $scope.newActivityForm;
+    data.discipline = data.selected_discipline.id;
+    console.log('add activity: ', data.date);
+    var splitDate = data.date.toString().split(' ');
+    data.date = splitDate[0] + ' ' + splitDate[1] + ' ' +splitDate[2] + ' ' +splitDate[3];
 
-		$http.post('/user/' + $rootScope.user._id + '/activities', data)
-		.success(function(res){
-			$scope.accordions.addActivity = false;
-			$scope.activities.unshift(res);
-		})
-		.error(function(e){});
-	}
+    $http.post('/user/' + $rootScope.user._id + '/activities', data)
+    .success(function(res){
+      $scope.accordions.addActivity = false;
+      $scope.activities.unshift(res);
+    })
+    .error(function(e){});
+  }
 
-	/*
-		deleteActivity function : calls the modal for delete activitiy confirmation
-		@param activity_id: the id of the specific activity
-	 */
-	$scope.deleteActivity = function( activity_id ){
-		$rootScope.confirm_delete_modal( activity_id, 'lg')
-		.then(function(result){
-			if(result){
-				for( var i in $scope.activities ) {
-					if( $scope.activities[i]._id == activity_id ){
-						$scope.activities.splice(i,1);
-						break;
-					}
-				}
-			}
-		});
+  /*
+    deleteActivity function : calls the modal for delete activitiy confirmation
+    @param activity_id: the id of the specific activity
+   */
+  $scope.deleteActivity = function( activity_id ){
+    $rootScope.confirm_delete_modal( activity_id, 'lg')
+    .then(function(result){
+      if(result){
+        for( var i in $scope.activities ) {
+          if( $scope.activities[i]._id == activity_id ){
+            $scope.activities.splice(i,1);
+            break;
+          }
+        }
+      }
+    });
 
-	}
-
-
-	/*
-		initEditableActivity function : initializes variables for editing an existing activity
-	 */
-	$scope.initEditableActivity = function( activity ){
-
-		activity.show_editable_form = !activity.show_editable_form;
-
-		$scope.updateActivityForm = {};
-		if( $scope.disciplines.time.indexOf( activity.discipline ) > -1) {
-			var splitted_performance = activity.performance.split(':');
-			//we get the existed performance in hh:mm:ss.cc format.
-				//We split it and add each part to the specific element
-			$scope.updateActivityForm.hours = splitted_performance[0].toString();
-			$scope.updateActivityForm.minutes = splitted_performance[1].toString();
-			$scope.updateActivityForm.seconds = splitted_performance[2].split('.')[0].toString();
-			$scope.updateActivityForm.centiseconds = splitted_performance[2].split('.')[1].toString();
-
-		}
-		else if( $scope.disciplines.distance.indexOf( activity.discipline ) > -1 ) {
-			$scope.updateActivityForm.distance_1 = (parseInt( activity.performance/10000 )).toString();
-			$scope.updateActivityForm.distance_2 = (parseInt( ( (activity.performance/10000) % $scope.updateActivityForm.distance_1 ) * 100 )).toString();
-		}
-		else if( $scope.disciplines.points.indexOf( activity.discipline ) > -1 ){
-			$scope.updateActivityForm.points = activity.performance;
-		}
-		else{
-			console.log('activity belongs to undefined type. WTF?');
-		}
-
-		$scope.updateActivityForm.date = activity.date;
-		$scope.updateActivityForm.place = activity.place;
-		$scope.updateActivityForm.location = activity.location;
-		$scope.updateActivityForm.competition = activity.competition;
-		$scope.updateActivityForm.notes = activity.notes;
-		$scope.updateActivityForm.private = activity.private;
-
-	}
+  }
 
 
-	/*
-		updateActivity function : edit a specific activity
-	 */
-	$scope.updateActivity = function( activity ){
-		var data = $scope.updateActivityForm;
-		console.log('clean', data);
+  /*
+    initEditableActivity function : initializes variables for editing an existing activity
+   */
+  $scope.initEditableActivity = function( activity ){
 
-		data.date = new Date(data.date.toString().split('T')[0]);
-		var splitDate = data.date.toString().split(' ');
-		data.date = splitDate[0] + ' ' + splitDate[1] + ' ' +splitDate[2] + ' ' +splitDate[3];
+    activity.show_editable_form = !activity.show_editable_form;
 
-		data.discipline = activity.discipline;
-		console.log('update activity: ', data);
-		$http.put( "/user/" + $rootScope.user._id + "/activities/" + activity._id, data)
-		.success( function(res){
-			console.log('res', res);
-			activity.formatted_performance = res.formatted_performance;
-			activity.formatted_date = res.formatted_date;
-			activity.place = res.place;
-			activity.location = res.location;
-			activity.competition = res.competition;
-			activity.notes = res.notes;
-			activity.private = res.private;
-			activity.show_editable_form = !activity.show_editable_form;
+    $scope.updateActivityForm = {};
+    if( $scope.disciplines.time.indexOf( activity.discipline ) > -1) {
+      var splitted_performance = activity.performance.split(':');
+      //we get the existed performance in hh:mm:ss.cc format.
+        //We split it and add each part to the specific element
+      $scope.updateActivityForm.hours = splitted_performance[0].toString();
+      $scope.updateActivityForm.minutes = splitted_performance[1].toString();
+      $scope.updateActivityForm.seconds = splitted_performance[2].split('.')[0].toString();
+      $scope.updateActivityForm.centiseconds = splitted_performance[2].split('.')[1].toString();
 
-		})
+    }
+    else if( $scope.disciplines.distance.indexOf( activity.discipline ) > -1 ) {
+      $scope.updateActivityForm.distance_1 = (parseInt( activity.performance/10000 )).toString();
+      $scope.updateActivityForm.distance_2 = (parseInt( ( (activity.performance/10000) % $scope.updateActivityForm.distance_1 ) * 100 )).toString();
+    }
+    else if( $scope.disciplines.points.indexOf( activity.discipline ) > -1 ){
+      $scope.updateActivityForm.points = activity.performance;
+    }
+    else{
+      console.log('activity belongs to undefined type. WTF?');
+    }
+
+    $scope.updateActivityForm.date = activity.date;
+    $scope.updateActivityForm.place = activity.place;
+    $scope.updateActivityForm.location = activity.location;
+    $scope.updateActivityForm.competition = activity.competition;
+    $scope.updateActivityForm.notes = activity.notes;
+    $scope.updateActivityForm.private = activity.private;
+
+  }
 
 
-	}
+  /*
+    updateActivity function : edit a specific activity
+   */
+  $scope.updateActivity = function( activity ){
+    var data = $scope.updateActivityForm;
+    console.log('clean', data);
+
+    data.date = new Date(data.date.toString().split('T')[0]);
+    var splitDate = data.date.toString().split(' ');
+    data.date = splitDate[0] + ' ' + splitDate[1] + ' ' +splitDate[2] + ' ' +splitDate[3];
+
+    data.discipline = activity.discipline;
+    console.log('update activity: ', data);
+    $http.put( "/user/" + $rootScope.user._id + "/activities/" + activity._id, data)
+    .success( function(res){
+      console.log('res', res);
+      activity.formatted_performance = res.formatted_performance;
+      activity.formatted_date = res.formatted_date;
+      activity.place = res.place;
+      activity.location = res.location;
+      activity.competition = res.competition;
+      activity.notes = res.notes;
+      activity.private = res.private;
+      activity.show_editable_form = !activity.show_editable_form;
+
+    })
+
+
+  }
 
 
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
