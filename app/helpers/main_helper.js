3074d3114334883560de9d251587b4d0ce05baa2
geobalas <geobal87@yahoo.gr>
2014-09-15 20:37:57 +0300

    Getting the profile by username

--- app/helpers/main_helper.js
+++ app/helpers/main_helper.js
@@ -17,18 +17,20 @@ mainHelper.validateAccess = function ( user_id, profile_id, callback ) {
 		user: {},
 		profile: {},
 		error: ''
-	};
-	
+	},
+	user_data = false;
+
 	if( user_id ) {
-		Profile.schema.findOne({ '_id': user_id }, '_id language date_format').then( function( user_data ) {
+		Profile.schema.findOne({ '_id': user_id }, '_id language date_format').then( function( user_data_response ) {
 
 			// If the user wasn't found, there's no point to continue
-			if( !user_data ) {
+			if( !user_data_response ) {
 				response.error = 'user_not_valid';
 				callback( response );
 			}
 
-			response.user = user_data;
+			response.user = user_data_response;
+			user_data = user_data_response;
 
 			// If the user didn't request profile specific data (E.g. he requested settings)
 			if( typeof profile_id === 'undefined' ) {
@@ -36,7 +38,7 @@ mainHelper.validateAccess = function ( user_id, profile_id, callback ) {
 				response.success = true;
 				callback( response );
 			} else {
-				access_profile( user_data );
+				accessProfile();
 			}
 		})
 		.fail( function( error ) {
@@ -44,38 +46,41 @@ mainHelper.validateAccess = function ( user_id, profile_id, callback ) {
 			callback( response );
 		});
 	} else {
+		// A user who is not logged in tries to access a profile
 		response.user = {
 			language: 		'en',
 			date_format: 	'd-m-y'
 		};
-		access_profile( user_data );
+		accessProfile();
 	}
 
-	function access_profile( user_data ) {
+	function accessProfile() {
 		// If there is no profile id, return an error
-		if( typeof profile_id === 'undefined' ) {
+		if( typeof profile_id === 'undefined' || !profile_id ) {
 			// Just return the user
 			response.error = 'profile_not_valid';
 			callback( response );
+			return;
 		}
 
 		// Find the profile by id
-		Profile.schema.findOne( { $or: [ {'_id': profile_id}, {'username': profile_id } ] }, '_id first_name last_name discipline country male picture private')
+		Profile.schema.findOne( { '_id': profile_id }, '_id first_name last_name discipline country male picture username private')
 		.then( function( profile_data ) {
 			// If the profile was found, get the data of the user
 			if( profile_data !== null && profile_data !== undefined ) {
-				// If the profile requested is a private profile and the user who tries to access it
-				// is not the user who owns it
-				if( profile_data.private && ( !user_data || profile_data._id.toString() !== user_data._id.toString() ) ) {
-					// Deny access
-					response.error = 'profile_not_valid';
-					callback( response );
-				} else {
-					// Return the profile data along with the data of the user who requested it
-					response.profile = profile_data;
-					response.success = true;
-					callback( response );
-				}
+				// Check if the profile found is private
+				handlePrivacy( profile_data );
+				return null;
+			} else {
+				// If the profile wasn't found, try to find it by username
+				return Profile.schema.findOne( { 'username': profile_id }, '_id first_name last_name discipline country male picture username private');
+			}
+		})
+		.then( function( profile_data ) {
+			// If the profile was found, get the data of the user
+			if( profile_data !== null && profile_data !== undefined ) {
+				// Check if the profile found is private
+				handlePrivacy( profile_data );
 			} else {
 				// If the profile wasn't found
 				response.error = 'profile_not_valid';
@@ -86,6 +91,21 @@ mainHelper.validateAccess = function ( user_id, profile_id, callback ) {
 			response.error = 'query_error';
 			callback( response );
 		});
+	}
+
+	function handlePrivacy( profile_data ) {
+		// If the profile requested is a private profile and the user who tries to access it
+		// is not the user who owns it
+		if( profile_data.private && ( !user_data || profile_data._id.toString() !== user_data._id.toString() ) ) {
+			// Deny access
+			response.error = 'profile_not_valid';
+			callback( response );
+		} else {
+			// Return the profile data along with the data of the user who requested it
+			response.profile = profile_data;
+			response.success = true;
+			callback( response );
+		}
 	}
 };
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,40 +5,6 @@ const Profile = require('../models/profile.js'),
 // Initialize translations
 const translations = require('../languages/translations.js');
 
-
-exports.get = function( req, res ) {
-  if( typeof req.session.user_id === 'undefined' ) {
-    res.json( null );
-    return false;
-  }
-
-  var user_id = req.session.user_id;
-  var user_data;
-
-  // Else, fetch the first name and the last name of the user from the database
-  Profile.schema.findOne({ '_id': user_id }, '_id first_name last_name discipline language')
-  .then( function( user_data ) {
-    if( user_data.first_name ) {
-
-      var tr = translations[user_data.language];
-
-      var data = {
-        'user': {
-          '_id':                  user_data._id,
-          'first_name':           user_data.first_name,
-          'last_name':            user_data.last_name,
-          'discipline':           user_data.discipline,
-          'formatted_discipline': tr[user_data.discipline]
-        }
-      };
-
-      res.json( data );
-    } else {
-      res.json( null );
-    }
-  });
-};
-
 exports.get_view = function( req, res ) {
   if( typeof req.session.user_id === 'undefined' ) {
     res.json( null );
