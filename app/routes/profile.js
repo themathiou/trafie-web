b81ffec6060ca803cc2b8f0e93f730693ca4f487
geobalas <geobal87@yahoo.gr>
2014-09-28 20:11:15 +0300

    Removed useless translations transferred with the profile data

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -237,37 +237,31 @@ exports.get_view = function( req, res ) {
 
   function render_profile() {
     var disciplines = {
-      'time': [
-        '100m',
-        '200m',
-        '400m',
-        '800m',
-        '1500m',
-        '3000m',
-        '60m_hurdles',
-        '100m_hurdles',
-        '110m_hurdles',
-        '400m_hurdles',
-        '3000m_steeple',
-        '4x100m_relay',
-        '4x400m_relay',
-        'marathon'
-      ],
-      'distance': [
-        'high_jump',
-        'long_jump',
-        'triple_jump',
-        'pole_vault',
-        'shot_put',
-        'discus',
-        'hammer',
-        'javelin'
-      ],
-      'points': [
-        'pentathlon',
-        'heptathlon',
-        'decathlon'
-      ]
+        '100m':           tr['100m'],
+        '200m':           tr['200m'],
+        '400m':           tr['400m'],
+        '800m':           tr['800m'],
+        '1500m':          tr['1500m'],
+        '3000m':          tr['3000m'],
+        '60m_hurdles':    tr['60m_hurdles'],
+        '100m_hurdles':   tr['100m_hurdles'],
+        '110m_hurdles':   tr['110m_hurdles'],
+        '400m_hurdles':   tr['400m_hurdles'],
+        '3000m_steeple':  tr['3000m_steeple'],
+        '4x100m_relay':   tr['4x100m_relay'],
+        '4x400m_relay':   tr['4x400m_relay'],
+        'marathon':       tr['marathon'],
+        'high_jump':      tr['high_jump'],
+        'long_jump':      tr['long_jump'],
+        'triple_jump':    tr['triple_jump'],
+        'pole_vault':     tr['pole_vault'],
+        'shot_put':       tr['shot_put'],
+        'discus':         tr['discus'],
+        'hammer':         tr['hammer'],
+        'javelin':        tr['javelin'],
+        'pentathlon':     tr['pentathlon'],
+        'heptathlon':     tr['heptathlon'],
+        'decathlon':      tr['decathlon']
     };
 
     // The data that will go to the front end
