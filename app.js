ef12b0d8555418362d941700c0bc68612c384da0
George Balasis <geobal87@yahoo.gr>
2015-10-20 00:30:15 +0300

    Fixed redis password

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ var index = require('./app/controllers/index'),
 var redisClient = null;
 if(db[process.env.NODE_ENV].redis.password) {
     redisClient = redis.createClient(db[process.env.NODE_ENV].redis.port, db[process.env.NODE_ENV].redis.host, {auth_pass: true});
-    redisClient.auth(db[process.env.NODE_ENV].password);
+    redisClient.auth(db[process.env.NODE_ENV].redis.password);
 } else {
     redisClient = redis.createClient(db[process.env.NODE_ENV].redis.port, db[process.env.NODE_ENV].redis.host);
 }
