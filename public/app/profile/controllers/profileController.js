44bbe14c7a6f7312524c9485488a5dfb6207c322
George Balasis <geobal87@yahoo.gr>
2016-01-14 22:57:09 +0200

    Working on the settings

--- public/app/profile/controllers/profileController.js
+++ public/app/profile/controllers/profileController.js
@@ -1,18 +1,11 @@
 (function(angular) {
     angular.module('trafie')
     .controller('ProfileController', function($rootScope, $scope, $routeParams, $location, Activity,
-                                              User, DISCIPLINES_MAP, userService) {
+                                              User, userService) {
         $scope.profileFound = true;
         $scope.loading = true;
         $scope.user = null;
         $scope.loadError = false;
-        $scope.disciplines = _.map(DISCIPLINES_MAP, function(translationKey, disciplineKey) {
-            return {
-                key: disciplineKey,
-                label: translationKey
-            };
-        });
-        $scope.selectedDiscipline = $scope.disciplines[0];
 
         if(!('userIdentifier' in $routeParams)) {
             userService.loadCurrentUser().then(loadProfile, function() {
--- public/app/settings/controllers/settingsController.js
+++ public/app/settings/controllers/settingsController.js
@@ -1,9 +1,11 @@
 (function(angular) {
     angular.module('trafie')
-    .controller('SettingsController', function($scope, $http, $window, userService, COUNTRIES, DISCIPLINES) {
+    .controller('SettingsController', function($scope, $http, $window, userService, COUNTRIES,
+                                               DISCIPLINES, LANGUAGES_MAP) {
         $scope.user = null;
         $scope.countries = [''].concat(COUNTRIES);
         $scope.disciplines = [''].concat(DISCIPLINES);
+        $scope.languages = LANGUAGES_MAP;
 
         userService.loadCurrentUser().then(function(user) {
             $scope.user = user;
