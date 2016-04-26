67870fe4d1b86182f26f7c0d5a6c37dd0ce36cf7
George Balasis <geobal87@yahoo.gr>
2016-04-26 21:59:53 +0300

    Last login and last ip data in the user's model

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -1,7 +1,12 @@
 'use strict';
+var user = require('../models/userModel')
 
 exports.post = function(req, res) {
     if (req.user) {
+        console.log(req.ip)
+        var ip = req.ip.substr(req.ip.lastIndexOf(':') + 1);
+        var now = new Date();
+        user.findOneAndUpdate(req.user._id, {lastLogin: now, lastIp: ip}, function(err, doc) {});
         res.status(200).json({'_id': req.user._id});
     } else {
         res.status(404).json({message: 'Resource not found', errors: [{
