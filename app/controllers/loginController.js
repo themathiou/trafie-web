86cbfc2a8176d5800c80c6df18bc20ead46ee926
George Balasis <geobal87@yahoo.gr>
2016-05-11 21:17:23 +0300

    Fix for lastIp

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -5,7 +5,7 @@ exports.post = function(req, res) {
     if (req.user) {
         var ip = req.ip.substr(req.ip.lastIndexOf(':') + 1);
         var now = new Date();
-        user.findOneAndUpdate(req.user._id, {lastLogin: now, lastIp: ip}, function(err, doc) {});
+        user.findOneAndUpdate({_id: req.user._id}, {lastLogin: now, lastIp: ip}, function(err, doc) {});
         res.status(200).json({'_id': req.user._id});
     } else {
         res.status(404).json({message: 'Resource not found', errors: [{
