eff36f069c14f31eddabdbea16ef299c2fe535ce
George Balasis <geobal87@yahoo.gr>
2016-05-17 19:42:07 +0300

    The feedback form can not be submitted again after the submit button is pressed

--- public/app/feedback/controllers/feedbackModalController.js
+++ public/app/feedback/controllers/feedbackModalController.js
@@ -7,6 +7,7 @@ angular.module('trafie')
         };
 
         $scope.send = function () {
+            $scope.sending = true;
             if(window.hasOwnProperty('navigator') && 'oscpu' in window.navigator) {
                 $scope.formData.osVersion = navigator.oscpu;
             }
@@ -15,6 +16,7 @@ angular.module('trafie')
             }
             $http.post('/feedback', $scope.formData)
                 .then(function(res) {
+                    $scope.sending = false;
                     if(res.status === 200) {
                         notify({
                             message: $filter('translate')('FEEDBACK.THANK_YOU_YOUR_FEEDBACK_HAS_BEEN_SENT'),
@@ -23,7 +25,7 @@ angular.module('trafie')
                         $uibModalInstance.close('close');
                     }
                 }, function(res) {
-
+                    $scope.sending = false;
                 });
         };
 
