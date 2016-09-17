fc710f40f2bb3de3f7cc127affbb1b332201db27
George Balasis <geobal87@yahoo.gr>
2016-09-18 02:49:48 +0300

    Changed the registration page

--- public/app/outer/controllers/appOuterController.js
+++ public/app/outer/controllers/appOuterController.js
@@ -1,8 +1,16 @@
 (function(angular) {
     angular.module('trafie-outer')
-        .controller('AppOuterController', function($scope, outerPageDataService) {
+        .controller('AppOuterController', function($scope, $location, outerPageDataService) {
             var self = this;
             self.getTitle = outerPageDataService.getTitle;
             self.getDescription = outerPageDataService.getDescription;
+            self.bodyClasses = [];
+            $scope.$on('$routeChangeSuccess', function() {
+                if($location.path() === '/register') {
+                    self.bodyClasses = ['register-page'];
+                } else {
+                    self.bodyClasses = ['app', 'on-canvas', 'nav-min', 'layout-horizontal', 'body-has-background', 'generic-outer-page'];
+                }
+            });
         });
 })(angular);
