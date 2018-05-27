24648c59393255c4775fb212243d3a4f74bbcdf9
Georgios Balasis <geobal87@yahoo.gr>
2018-05-27 20:32:06 +0300

    Fix the size of the badges

--- public/app/outer/directives/availableOnBadgesDirective.js
+++ public/app/outer/directives/availableOnBadgesDirective.js
@@ -2,13 +2,14 @@ angular.module('trafie-outer')
     .directive('availableOnBadges', function () {
         return {
             restrict: 'E',
-            template: '<span class="available-on-badges-container">' +
+            replace: true,
+            template: '<div class="available-on-badges-container">' +
                     '<a class="available-on-badge-link" target="_blank" href="https://itunes.apple.com/cg/app/trafie/id1055761534">' +
                         '<img class="available-on-badge-image available-on-badge-image-ios" src="/images/outer/Download_on_the_App_Store_Badge_US-UK_135x40.svg">' +
                     '</a>' +
                     '<a class="available-on-badge-link" target="_blank" href="https://play.google.com/store/apps/details?id=com.trafie">' +
                         '<img class="available-on-badge-image" src="/images/outer/google-play-badge.png">' +
                     '</a>' +
-            '</span>'
+            '</div>'
         };
     });
