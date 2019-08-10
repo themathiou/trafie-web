3fc40c20d79281661c2634f77bf19ae56062a1cb
Georgios Balasis <geobal87@yahoo.gr>
2019-08-11 02:22:22 +0300

    Add featured profiles

--- public/app/outer/controllers/registerController.js
+++ public/app/outer/controllers/registerController.js
@@ -45,6 +45,7 @@
             server: 'There was a problem processing your request. Please try later.'
         };
 
+        $scope.disciplinesMap = {"60M":"60m","100M":"100m","200M":"200m","400M":"400m","800M":"800m","1500M":"1500m","3000M":"3000m","5000M":"5000m","10000M":"10000m","60M_HURDLES":"60m hurdles","100M_HURDLES":"100m hurdles","110M_HURDLES":"110m hurdles","400M_HURDLES":"400m hurdles","3000M_STEEPLECHASE":"3000m steeplechase","4X100M_RELAY":"4x100m relay","4X400M_RELAY":"4x400m relay","HALF_MARATHON":"Half marathon","MARATHON":"Marathon","20KM_RACE_WALK":"20km race walk","50KM_RACE_WALK":"50km race walk","CROSS_COUNTRY_RUNNING":"Cross country running","HIGH_JUMP":"High jump","LONG_JUMP":"Long jump","TRIPLE_JUMP":"Triple jump","POLE_VAULT":"Pole vault","SHOT_PUT":"Shot put","DISCUS":"Discus","HAMMER":"Hammer","JAVELIN":"Javelin","PENTATHLON":"Pentathlon","HEPTATHLON":"Heptathlon","DECATHLON":"Decathlon"};
         var disciplines = ['60m','100m','200m','400m','800m','1500m','3000m','5000m','10000m','60m hurdles','100m hurdles','110m hurdles','400m hurdles','3000m steeplechase','4x100m relay','4x400m relay','Half marathon','Marathon','20km race walk','50km race walk','Cross country running','High jump','Long jump','Triple jump','Pole vault','Shot put','Discus','Hammer','Javelin','Pentathlon','Heptathlon','Decathlon']
         if ($("#type-disciplines").length) {
             new window.Typed("#type-disciplines", {
@@ -54,6 +55,18 @@
                 loop: true,
                 shuffle: true,
             });
+        }
+        var userIds = ["574544560d5ed80300c7dc51", "5d261e73661628002cbb9758", "5b30a66f30b4900400371c73", "5d32eb940fb6ca002cc7f16a"];
+        $scope.featuredUsers = [];
+        if ($("#featured-atheletes").length) {
+            for (var i = 0 ; i < userIds.length ; i++) {
+                $http.get("/users/" + userIds[i])
+                    .then(function(res) {
+                        if (res.status === 200) {
+                            $scope.featuredUsers.push(res.data);
+                        }
+                    });
+            }
         }
         
         var $shape = $('.shape-container');
